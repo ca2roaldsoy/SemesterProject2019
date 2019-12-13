6b5afb05c8ac9eab3eaed41f1ca184567af903a0
ca2roaldsoy <roaldsoyen@gmail.com>
2019-12-13 21:45:29 +0100

    added play again button. added player score

--- winner.js
+++ winner.js
@@ -1,8 +1,8 @@
 function init () {
 
 var canvas = document.getElementById('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 200;
+canvas.height = 200;
 var context = canvas.getContext('2d');
 var particles = [];
     
@@ -36,14 +36,14 @@ function anim() {
     
     for (var i = 0; i < particles.length; i++){
     var p = particles[i];
-    context.fillStyle = `rgba(0, 255, 0, ${p.opacity}`;
-    context.fillRect(p.x, p.y, p.size, p.size);
+    context.fillStyle = "red"//`rgba(0, 255, 0, ${p.opacity}`;
+    context.fillRect(p.x, p.y+50, p.size, p.size);
     p.x = p.x + Math.cos(p.velocityX - 100);
     p.y = p.y + Math.sin(p.velocityY + 200);
     
   }
         
-    p.opacity -= i/100;
+    //p.opacity -= i/100;
     
  }
     var interval = setInterval(anim);
@@ -59,15 +59,49 @@ else {
     
     alert("your browser does not support html canvas")
 }
+var scoreBtn = document.getElementById("scoreBtn");
+var playerScore = document.querySelector(".playerScore");
+var playAgainBtn = document.querySelector("#playAgainBtn");
+var tokenImg = document.querySelector(".tokenImg");
+var getPlayerScore;
+var getplayerName;
 
-/*var playerName = document.querySelector(".playerName");
-var getLocal;
-
-function getStorage(arg1) {
+function getStorage(arg1, arg2) {
     
-    getLocal = localStorage.getItem(arg1);
-    playerName.innerHTML = getLocal;
+    getPlayerScore = localStorage.getItem(arg1);
+    getplayerName = localStorage.getItem(arg2);
+    playerScore.innerHTML += getPlayerScore;
     
 };
 
-getStorage("player");*/
+getStorage("playerScore", "player");
+
+
+scoreBtn.addEventListener("click", function() {
+    
+    scoreBtn.style.display = "none";
+    playerScore.style.display = "block";
+})
+
+playAgainBtn.addEventListener("click", function(){
+    
+    window.location.href = "index.html"
+})
+
+
+var tokenImgEl = document.createElement("img")
+tokenImgEl.classList.add("tokenImg")
+
+if (getplayerName == "Arya Stark") {
+    
+    tokenImgEl.src = "images/Tokens/arya_stark.png";
+    tokenImgEl.alt = "Arya Stark";
+    tokenImg.appendChild(tokenImgEl);
+}
+
+if (getplayerName == "Jaime Lannister") {
+    
+    tokenImgEl.src = "images/Tokens/jamie_lannister.png";
+    tokenImgEl.alt = "Jaime Lannister";
+    tokenImg.appendChild(tokenImgEl);
+}
